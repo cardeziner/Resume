93410d7689529735f123662e7ed67d8e0c8ed9b6
Peter Stevens <cardeziner@gmail.com>
2020-07-15 01:07:11 -0400

    added bootstrap and sample

--- app/javascript/react/components/HomePageContainer.js
+++ app/javascript/react/components/HomePageContainer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ProfileContainer from './ProfileContainer'
 import SampleBox from './SampleBox'
 import image from '../../../assets/images/profilepic.png'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const HomePageContainer = (props) =>{
   return(
@@ -9,7 +10,23 @@ const HomePageContainer = (props) =>{
       <div className="profile-box">
       <h2 className="title-1">About Me</h2>
       <img src={image}/>
+      <div>
+        <h2 className="text-1">
+          Peter Stevens
+        </h2>
       </div>
+      </div>
+      <Container>
+  <Row>
+    <Col className="col-color">1 of 2</Col>
+    <Col>2 of 2</Col>
+  </Row>
+  <Row>
+    <Col>1 of 3</Col>
+    <Col>2 of 3</Col>
+    <Col>3 of 3</Col>
+  </Row>
+</Container>
     </div>
   )
 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Jobs.create(title:, location:, start_date:, end_date:, duties:[])
+Jobs.create(title:, location:, start_date:, end_date:, duties:[])
+Jobs.create(title:, location:, start_date:, end_date:, duties:[])
+Jobs.create(title:, location:, start_date:, end_date:, duties:[])
