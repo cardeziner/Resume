1e48ea83b78b2ff608b30eb0722613443d81d476
Peter Stevens <cardeziner@gmail.com>
2020-07-22 13:48:32 -0400

    added new styling

--- app/javascript/react/components/HomePageContainer.js
+++ app/javascript/react/components/HomePageContainer.js
@@ -6,28 +6,25 @@ import { Container, Row, Col } from 'react-bootstrap'
 
 const HomePageContainer = (props) =>{
   return(
-    <div>
-      <Container>
-        <Row>
-          <div>
-            <div className="profile-box">
-              <h2 className="western">Welcome</h2>
-              <img src={image}/>
-            <div>
-              <h1 className="western">
-                Peter Stevens
-              </h1>
+    <div className="row spacing">
+      <div className="col-4">
+        <div className="top border">
+          <h1 className="bold">personal details</h1>
+        </div>
+        <img src={image}/>
+          <div className="border">
+            <div className="row">
+              <div className="column"><h2 className="bold left">age</h2><br></br><h2 className="bold left">location</h2></div>
+              <div className="column right"><h4 className="soft right">30</h4><br></br><h4 className="soft right">Denver Colorado</h4><br></br></div>
             </div>
           </div>
-        </div>
-        </Row>
-      <Row>
-        <Col>
-        </Col>
-      </Row>
-    </Container>
-  </div>
+          </div>
+      <div className="col-8">Hello</div>
+    </div>
   )
 }
 
 export default HomePageContainer
+// <h2 className="bold center">Peter Stevens</h2>
+// <h2 className="bold left center">age</h2><h4 className="soft right">30<br></br>hello</h4>
+// <h2 className="left left">hello</h2>
