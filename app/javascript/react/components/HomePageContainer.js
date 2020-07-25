f62443572cc24f55c6195a8844901c7aed5f0056
Peter Stevens <cardeziner@gmail.com>
2020-07-24 21:06:24 -0400

    added github link

--- app/javascript/react/components/HomePageContainer.js
+++ app/javascript/react/components/HomePageContainer.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ProfileContainer from './ProfileContainer'
 import SampleBox from './SampleBox'
-import image from '../../../assets/images/profilepic.png'
+import image1 from '../../../assets/images/profilepic.png'
+import image2 from '../../../assets/images/github.png'
 import { Container, Row, Col } from 'react-bootstrap'
 
 const HomePageContainer = (props) =>{
@@ -11,13 +12,17 @@ const HomePageContainer = (props) =>{
         <div className="top border">
           <h1 className="bold">personal details</h1>
         </div>
-        <img className="bottom top" src={image}/>
+        <img className="bottom top" src={image1}/>
           <div className="border bottom">
             <div className="row col">
               <div className="col-6"><h3 className="bold center">age</h3><h4 className="soft center">30 y/o</h4></div>
               <div className="col-6"><h3 className="bold left">location</h3><h4 className="soft center">Denver</h4></div>
               <div className="col-12"><h3 className="bold left center">learning institution</h3></div>
               <div className="col-12"><h4 className="soft left center">Launch Academy, Boston</h4></div>
+              <div className="col-3"><img href="http://www.github.com/cardeziner" src={image2}/></div>
+              <div className="col-3"></div>
+              <div className="col-3"></div>
+              <div className="col-3"></div>
             </div>
           </div>
         </div>
