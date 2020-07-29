fb2b81e5fb12db0bdeae7945e21728f20a88786f
Peter Stevens <cardeziner@gmail.com>
2020-07-29 01:07:56 -0400

    about me added

--- app/javascript/react/components/HomePageContainer.js
+++ app/javascript/react/components/HomePageContainer.js
@@ -6,6 +6,8 @@ import github from '../../../assets/images/github.png'
 import linkedin from '../../../assets/images/linkedin.png'
 import facebook from '../../../assets/images/facebook.png'
 import launch from '../../../assets/images/launch.png'
+import aboutme from '../../../assets/images/aboutme.jpeg'
+
 import { Container, Row, Col } from 'react-bootstrap'
 
 
@@ -33,7 +35,16 @@ const HomePageContainer = (props) =>{
           </div>
         </div>
       <div className="col-8 borders">
-      <h1 className="bold">Hello</h1></div><hr className="center"/>
+        <div>
+          <h1 className="bold padding-top aboutme">about me</h1><hr className="center"/>
+          <h5 className="inline-block">Hello! My name is Peter Stevens and I am originally from Boston, MA. In my spare time, I like to go on hikes, I love camping, and I love just spending time outdoors, so its no suprise how I have recently just moved back to Denver!  I have recently graduated from a full-stack development boot camp program at Launch Academy, and I am very excited to begin a new career in software development. As I am looking for new opportunities and potential teams to join to grow my knowledge and expand my skills, I have been also trying to find a role that also excites me. I am currently looking for an opportunity where I am able to expand my skills with similar technologies I have already become accustomed to using and looking for opportunities to learn new technologies as well! As I have been working on improving my skills throughout my time since graduation, I have worked to learn new technologies and coding strategies which include working with Express, improving my front-end capabilities, and I have also been attending a number of tech events to stay in the loop with whats going on in the industry, and to learn about new ways to implement my code! I would like to thank you for taking the time to look at my website, and I look forward to you getting to learn a little more about me! </h5><hr className="center"/>
+        </div>
+          <div className="row">
+            <div className="col-4"><img src={aboutme}/></div>
+            <div className="col-4"><img src={aboutme}/></div>
+            <div className="col-4"><img src={aboutme}/></div>
+          </div>
+      </div>
     </div>
   )
 }
