80663e0d13c1c45e112cfc3d101d5e32f1773bf6
Peter Stevens <cardeziner@gmail.com>
2020-08-03 18:20:36 -0400

    link boxes

--- app/javascript/react/components/HomePageContainer.js
+++ app/javascript/react/components/HomePageContainer.js
@@ -37,14 +37,16 @@ const HomePageContainer = (props) =>{
       <div className="col-8 borders">
         <div>
           <h1 className="bold padding-top aboutme">about me</h1><hr className="center"/>
-          <h5 className="inline-block">Hello! My name is Peter Stevens and I am originally from Boston, MA. In my spare time, I like to go on hikes, I love camping, and I love just spending time outdoors, so its no suprise how I have recently just moved back to Denver!  I have recently graduated from a full-stack development boot camp program at Launch Academy, and I am very excited to begin a new career in software development. As I am looking for new opportunities and potential teams to join to grow my knowledge and expand my skills, I have been also trying to find a role that also excites me. I am currently looking for an opportunity where I am able to expand my skills with similar technologies I have already become accustomed to using and looking for opportunities to learn new technologies as well! As I have been working on improving my skills throughout my time since graduation, I have worked to learn new technologies and coding strategies which include working with Express, improving my front-end capabilities, and I have also been attending a number of tech events to stay in the loop with whats going on in the industry, and to learn about new ways to implement my code! I would like to thank you for taking the time to look at my website, and I look forward to you getting to learn a little more about me! </h5>
-          <h3 className="bold">to learn more about myself and my coding experience so far, feel free to visit my <a href="https://launchpass.launchacademy.com/users/PeterStevens/launcher_profile"> launcher profile</a>!</h3>
+          <h5 className="inline-block">Hello! My name is Peter Stevens and I am originally from Boston, MA, but have recently moved back to Denvert. In my spare time, I like to go on hikes, I love camping, and I love just spending time outdoors, so its no suprise how I have recently just moved back to Denver!  I have recently graduated from a full-stack development boot camp program at Launch Academy, and I am very excited to begin a new career in software development.</h5>
+          <h5 className="inline-block">As I am looking for new opportunities and potential teams to join to grow my knowledge and expand my skills, I have been also trying to find a role that also excites me. I am currently looking for an opportunity where I am able to expand my skills with similar technologies I have already become accustomed to using and looking for opportunities to learn new technologies as well</h5>
+          <h5 className="inline-block">As I have been working on improving my skills throughout my time since graduation, I have worked to learn new technologies and coding strategies which include working with Express, improving my front-end capabilities, and I have also been attending a number of tech events to stay in the loop with whats going on in the industry, and to learn about new ways to implement my code. I would like to thank you for taking the time to look at my website, and I look forward to you getting to learn a little more about me! to learn more about myself and my coding experience so far, feel free to visit my <a href="https://launchpass.launchacademy.com/users/PeterStevens/launcher_profile"> Launcher Profile</a>!</h5>
+
           <hr className="center"/>
         </div>
         <div className="row">
-          <div className="col-4"><img src={aboutme}/></div>
-          <div className="col-4"><img src={aboutme}/></div>
-          <div className="col-4"><img src={aboutme}/></div>
+          <div className="col-4"><h1 className="yellow-box center">Work History</h1></div>
+          <div className="col-4"><h1 className="center green-box">Education</h1></div>
+          <div className="col-4 "><h1 className="center orange-box">Projects</h1></div>
         </div>
       </div>
     </div>
