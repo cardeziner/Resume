898909591880e9510acc019b347944539dcf5a2a
Peter Stevens <cardeziner@gmail.com>
2020-08-12 15:04:19 -0600

    education updates

--- app/javascript/react/components/EducationComponent.js
+++ app/javascript/react/components/EducationComponent.js
@@ -15,19 +15,29 @@ const EducationComponent = (props) =>{
             <div className="inline-block">
               <div className="row">
                 <div className="col-6">
-                  <h4 className="bold">degree</h4>
-                  <p className="soft">Industrial Design</p>
+                  <h4 className="bold">location</h4>
+                  <p className="soft">Denver, CO</p>
                 </div>
                 <div className="col-6">
                   <h4 className="bold">years attended</h4>
                   <p className="soft">2014 - 2017</p>
                 </div>
               </div>
+              <div className="row">
+                <div className="col-12">
+                  <h4 className="bold">degree</h4>
+                  <p className="soft">Industrial Design</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
         <div className="col-6">
-          <div className="borders gap"><h1>hello</h1>
+          <div className="borders"><br/>
+            <img className="circles" src={launchlogo}/>
+            <h3 className="bold"><br/>Launch Academy - Full Stack Development Boot Camp</h3>
+            <hr className="center"/>
+            <h1>hello</h1>
           </div>
         </div>
       </div>
