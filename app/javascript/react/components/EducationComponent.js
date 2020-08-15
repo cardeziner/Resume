f9562b78eabde6229daddda4ab8c07a7074d5aff
Peter Stevens <cardeziner@gmail.com>
2020-08-15 17:19:24 -0600

    updated education

--- app/javascript/react/components/EducationComponent.js
+++ app/javascript/react/components/EducationComponent.js
@@ -9,7 +9,7 @@ const EducationComponent = (props) =>{
       <div className="row spacing gap">
         <div className="col-6 ">
           <div className="borders"><br/>
-            <img className="circles" src={msulogo}/>
+            <a href="http://www.msudenver.edu"><img className="circles" src={msulogo}/></a>
             <h3 className="bold"><br/>Metropolitan State University of Denver</h3>
             <hr className="center"/>
             <div className="inline-block">
@@ -25,7 +25,7 @@ const EducationComponent = (props) =>{
               </div>
               <div className="row">
                 <div className="col-12">
-                  <h4 className="bold">degree</h4>
+                  <h4 className="bold">curriculum / area of study</h4>
                   <p className="soft">Industrial Design</p>
                 </div>
               </div>
@@ -37,7 +37,7 @@ const EducationComponent = (props) =>{
         </div>
         <div className="col-6">
           <div className="borders"><br/>
-            <img className="circles" src={launchlogo}/>
+            <a href="http://www.launchacademy.com"><img className="circles" src={launchlogo}/></a>
             <h3 className="bold"><br/>Launch Academy - Full Stack Boot Camp</h3>
             <hr className="center"/>
             <div className="inline-block">
@@ -60,7 +60,15 @@ const EducationComponent = (props) =>{
             </div>
             <hr className="center"/>
             <h4 className="bold center">what i learned </h4>
-            <p className="inline-block soft">As someone who never coded before, I managed to get a lot out of my experience at Launch Academy.</p>
+            <p className="inline-block soft">I am a recent graduate from Launch Academy, one of the top coding schools in Boston. I have always had an interest in computers and web design, so it only made sense that I finally took the steps to become an experienced developer. I learned how to do full stack programming and learned a variety of languages and technology. Now that I have the skills I need to succeed, I am seeking a team that will help me learn even more, one that will allow me to utilize the new skills I have learned, and one that will allow me to expand on my skills!</p>
+            <hr className="center"/>
+            <h4 className="bold center">Specialties</h4>
+            <p classNamer="inline-block no-bull">
+              <li>Ruby</li>
+              <li>JavaScript</li>
+              <li>HTML</li>
+              <li>CSS</li>
+            </p>
           </div>
         </div>
       </div>
