f3818cc7a103513783124b9db9c222ba7cf9755d
Peter Stevens <cardeziner@gmail.com>
2020-08-26 12:17:34 -0600

    image corrections

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= //= require foundation
-// require_tree .
+//= require foundation
+//= require_tree .
 
 $(function(){ $(document).foundation(); });
 
